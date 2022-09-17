5035282cae587c89919fac9ad95f09231916209c
Ryan Porterfield <ryan@ryanporterfield.com>
2022-09-16 20:32:30 -0600

    Upgrade dependencies
    - Add ClashAPI dependency
    - Upgrade Discord4J version
    - Upgrade Jackson version
    - Upgrade Logback version
    - Upgrade Netty version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,25 +21,27 @@ repositories {
 dependencies {
     // KSP
     ksp("io.insert-koin:koin-ksp-compiler:1.0.1")
+    // Clash API
+    implementation("com.lycoon:clashapi:3.0.2")
     // Discord4J
-    implementation("com.discord4j:discord4j-core:3.2.2")
+    implementation("com.discord4j:discord4j-core:3.2.3")
     // Google Guava
     implementation("com.google.guava:guava:31.1-jre")
     // Jackson
-    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.4")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.4")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
+    implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:2.13.4")
     // Koin
     implementation("io.insert-koin:koin-annotations:1.0.1")
     implementation("io.insert-koin:koin-core:3.2.0")
     // Kotlin coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     // Logback
-    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation("ch.qos.logback:logback-classic:1.4.0")
     // Netty
-    implementation("io.netty:netty-common:4.1.79.Final")
+    implementation("io.netty:netty-common:4.1.81.Final")
 
     // Kotlin Test
     testImplementation(kotlin("test"))
@@ -48,6 +50,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
     // Koin Test
     testImplementation("io.insert-koin:koin-test:3.2.0")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 application {
@@ -65,4 +68,12 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
